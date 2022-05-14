3ecfc616e36367bc7e84fed0b0e60edcee4a6651
shreyasajoshi123 <80787020+shreyasajoshi123@users.noreply.github.com>
2022-05-14 22:50:21 +0530

    Initial commit

---
+++ users/admin.py
@@ -0,0 +1,6 @@
+from atexit import register
+from django.contrib import admin
+
+# Register your models here.
+from .models import Profile
+admin.site.register(Profile)
---
+++ users/apps.py
@@ -0,0 +1,9 @@
+import imp
+from django.apps import AppConfig
+
+
+class UsersConfig(AppConfig):
+    name = 'users'
+
+    def ready(self):
+        import users.signals
